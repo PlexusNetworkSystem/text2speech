23ccda803ef5122e6a3696058b2f014ab0f7b672
Plexus Network Team <100467384+PlexusNetworkSystem@users.noreply.github.com>
2022-09-13 12:56:18 +0300

    Add files via upload

---
+++ en.tts-engine.rb
@@ -0,0 +1,8 @@
+# require 'tts'
+#def say(word)
+#    s = "#{word}"
+#    repeatTimes = 1
+#    s.play("en", repeatTimes)
+#end
+#speak = File.read("/usr/share/Plexus.Network/Asistan/tts/en/speech.word.en")
+#say("#{speak}")
---
+++ tr.tts-engine.rb
@@ -0,0 +1,8 @@
+#require 'tts'
+#def say(word)
+#    s = "#{word}"
+#    repeatTimes = 1
+#    s.play("tr", repeatTimes)
+#end
+#speak = File.read("/usr/share/Plexus.Network/Asistan/tts/tr/speech.word.tr")
+#say("#{speak}")
